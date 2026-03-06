730ba04413b2da96ee4ba0443130d091903e16e4
santiago gutierrez <santiagogutierrezhernandez1012@gmail.com>
2026-03-05 23:51:58 -0300

    Refactor Spikes collision handling to use Dictionary for damage tracking and damage cooldown

--- RoguelikeFNA/Scripts/Components/LevelElements/Spikes.cs
+++ RoguelikeFNA/Scripts/Components/LevelElements/Spikes.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Nez;
 
 namespace RoguelikeFNA.LevelElements
@@ -8,16 +9,17 @@ namespace RoguelikeFNA.LevelElements
     public class Spikes : Component, IUpdatable, ITriggerListener
     {
         public int Damage = 1;
-        HashSet<Collider> _currentCollisions = new();
+        Dictionary<Collider, float> _currentCollisions = new();
+        const float DAMAGE_INTERVAL = 0.5f;
 
         public void OnTriggerEnter(Collider other, Collider local)
         {
             if(
-                !_currentCollisions.Contains(other)
+                !_currentCollisions.ContainsKey(other)
                 && other.Entity.HasComponent<EntityStats>()
                 && other.HasComponent<HealthController>()
             )
-                _currentCollisions.Add(other);
+                _currentCollisions.Add(other, DAMAGE_INTERVAL);
         }
 
         public void OnTriggerExit(Collider other, Collider local)
@@ -27,14 +29,32 @@ namespace RoguelikeFNA.LevelElements
 
         public void Update()
         {
-            _currentCollisions.RemoveWhere(c => !c.Enabled || c.Entity is null || c.Entity.IsDestroyed);
-            foreach(var collider in _currentCollisions)
+            _currentCollisions.Keys.Where(
+                c => !c.Enabled || c.Entity is null || c.Entity.IsDestroyed
+            ).ToList().ForEach(c => _currentCollisions.Remove(c));
+            foreach(var collider in _currentCollisions.Keys.ToList())
             {
-                var stats = collider.Entity.GetComponent<EntityStats>();
-                if(stats != null && !stats.IsIntangible && !stats.IsInvincible)
+                if(!_currentCollisions.ContainsKey(collider))
+                    continue;
+
+                if(_currentCollisions[collider] >= DAMAGE_INTERVAL)
                 {
-                    collider.Entity.GetComponent<HealthController>()?.Hit(Damage, Entity);
+                    _currentCollisions[collider] = 0f;
+                    ApplyDamage(collider);
                 }
+                else
+                {
+                    _currentCollisions[collider] += Time.DeltaTime;
+                }
+            }
+        }
+
+        void ApplyDamage(Collider collider)
+        {
+            var stats = collider.Entity.GetComponent<EntityStats>();
+            if(stats != null && !stats.IsIntangible && !stats.IsInvincible)
+            {
+                collider.Entity.GetComponent<HealthController>()?.Hit(Damage, Entity);
             }
         }
     }
